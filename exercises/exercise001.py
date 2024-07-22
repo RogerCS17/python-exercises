44a078fdb89ad51f3ae210de9951ec6fc7982f10
RogerCS17 <rogercs1704@gmail.com>
2024-07-22 16:37:52 -0500

    solution: exercise001

---
+++ exercises/exercise001.py
@@ -0,0 +1,10 @@
+# Sumar dos números y mostrar su resultado
+# Se declara las variables
+number01 = 4
+number02 = 10
+
+# Variable que contendrá el resultado
+resultado = number01 + number02
+
+# Se muestra el resultado
+print(f"La suma es: {resultado}")
